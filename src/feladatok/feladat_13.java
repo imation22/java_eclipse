c0d72c35b7046908659aac8c1da660972f9d3a53
imation22 <33782674+imation22@users.noreply.github.com>
2018-06-27 17:24:45 +0200

    new

---
+++ src/feladatok/feladat_13.java
@@ -0,0 +1,32 @@
+package feladatok;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+//13.	Írj egy programot, mely szöveg file beolvasása során megállapítja melyik karakter hányszor fordult elõ a file-ban! (javasolt megoldás: HashTable)
+public class feladat_13 {
+
+	public static void main(String[] args) {
+		String teszt= "ez egy teszt";
+		String teszt1 = "bátorság";
+		int szamlalo = 0;
+		
+		 Hashtable<Character, Integer> hm = new Hashtable<Character, Integer>();
+		 for (char ch : teszt.toCharArray())
+			 hm.put(ch, hm.containsKey(ch) ? (hm.get(ch) + 1) : 1);
+
+			System.out.println(hm);
+
+			 
+		    /*Set<Map.Entry<Character, Integer>> set = hm.entrySet();
+
+		    for (Map.Entry<Character, Integer> me : set) {
+		      System.out.print(me.getKey() + ": ");
+		      System.out.println(me.getValue());
+		    }*/
+
+	}
+
+}
---
+++ src/feladatok/feladat_23_v2.java
@@ -0,0 +1,26 @@
+package feladatok;
+
+
+//23.	Írj egy függvényt, mely két sztringet kap paraméterként és visszaadja hány karakterük egyezik meg! (például: „alma” és „álmatlan” -> 2 ; „sátortábor” és „bátorság” -> 5 ; „ágy”,„vágy” ->0
+public class feladat_23_v2 {
+
+	public static void main(String[] args) {
+		String egy = "alma";
+		String ketto = "álmatlan";
+		int pozicio = 0;
+		int count = 0;
+		String teszt = "";
+		teszt.toCharArray();
+		
+		teszt = egy +ketto;
+		System.out.println(teszt);
+
+		
+		
+		
+	}
+}
+	
+
+
+
